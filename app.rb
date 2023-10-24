ae617a5ac4b0178ac20abbf4c0ae698f0ca7eff0
tinacho12 <146214538+tinacho12@users.noreply.github.com>
2023-10-24 19:00:20 +0000

    Fixed subtract bugs

--- app.rb
+++ app.rb
@@ -21,9 +21,9 @@ get("/subtract") do
 end
 
 get("/wizard_subtract") do
-  @first_num = params.fetch("first_num").to_f
-  @second_num = params.fetch("second_num").to_f
-
+  @first_num = params.fetch("first_number").to_f
+  @second_num = params.fetch("second_number").to_f
+  @subtract_result = @second_num - @first_num
   erb(:sub_result)
 end
 
